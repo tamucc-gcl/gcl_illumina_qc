75c4aaea855f5fb0b7d198719a5124d369e44cd1
Jason Selwyn <jdselwyn@gmail.com>
2026-03-31 11:56:02 -0400

    updates

--- scratch/insert_hist.R
+++ scratch/insert_hist.R
@@ -7,20 +7,38 @@ insert_data <- list.files(path = '~/../Downloads/insert_hist',
              show_col_types = FALSE) %>%
   mutate(sample_id = basename(sample_id) %>%
            str_remove('.insert_hist.tsv')) %>%
-  filter(number > 0,
-         insert_size < 1000)
+  filter(number > 0) %>%
+  filter(insert_size < 1000)
+
+mean_sizes <- dplyr::summarize(insert_data, 
+                               total_n = sum(number),
+                               mean_size = sum(insert_size * number) / sum(number),
+                               sd_size = sqrt(sum(number * (insert_size - mean_size)^2) / (total_n - 1)),
+                               .by = sample_id)
 
 insert_data %>%
-  ggplot(aes(x = insert_size, 
-             weight = number)) +
-  geom_histogram() +
+  ggplot(aes(x = insert_size)) +
+  geom_histogram(aes(weight = number)) +
+  geom_vline(data = mean_sizes,
+             aes(xintercept = mean_size + -1 * sd_size),
+             linetype = 'dashed') +
+  geom_vline(data = mean_sizes,
+             aes(xintercept = mean_size + 1 * sd_size),
+             linetype = 'dashed') +
+  geom_vline(data = mean_sizes,
+             aes(xintercept = mean_size)) +
+  geom_label(data = mean_sizes,
+            aes(label = str_c("Mean Insert Size\n", scales::comma(mean_size)),
+                x = mean_size + 1 * sd_size), 
+            y = Inf, vjust = 1.5,
+            hjust = 0, border.colour = 'white') +
   theme(x = 'Insert Size',
-        y = 'Number')
+        y = 'Number') +
+  facet_wrap(~sample_id,
+             scales = 'free_y') +
+  theme_classic() +
+  theme(panel.background = element_rect(colour = 'black'),
+        strip.background = element_blank())
 
 
-dplyr::summarize(insert_data, mean_size = sum(insert_size * number) / sum(number))
 
-read_delim('~/../Downloads/mean_insert_sizes.tsv',
-           show_col_types = FALSE) %>%
-  ggplot(aes(x = mean_insert_size)) +
-  geom_histogram()
